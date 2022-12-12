d9332475d50a26a6809cef9dab4f94c1f6f8bfbd
SC6 <98927145+s0984094@users.noreply.github.com>
2022-12-12 14:31:18 -0500

    Create ShapeException.java

---
+++ ShapeException.java
@@ -0,0 +1,14 @@
+package edu.monmouth.shape;
+
+public class ShapeException extends Exception {
+
+    public ShapeException() {
+	super();
+    }
+
+    public ShapeException(String message) {
+	super(message);
+    }
+
+  
+}
